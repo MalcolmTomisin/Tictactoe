5238016ec4b67b2f05e261fb7f08817056b26d30
Tomi Alu <malcolmtomisin@gmail.com>
2025-05-13 14:31:49 +0100

    refactor: extract inline styles

--- src/screens/MenuScreen/MenuScreenContent.tsx
+++ src/screens/MenuScreen/MenuScreenContent.tsx
@@ -8,61 +8,65 @@ export default function MenuScreenContent({
 }: {
   navigation: NativeStackNavigationProp<RootScreenProps, 'Menu'>;
 }) {
+  const handleButtonPress = (player: 'X' | 'O') => {
+    navigation.navigate('Home', { player });
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Image
         source={{
           uri: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Tic_tac_toe.svg',
         }}
-        style={{
-          alignSelf: 'center',
-          width: 150,
-          height: 150,
-        }}
+        style={styles.centeredImage}
         accessibilityIgnoresInvertColors
+        accessibilityRole="image"
+        accessible
       />
       <View>
         <TouchableOpacity
           accessibilityRole="button"
-          style={{
-            backgroundColor: '#FF6EC7',
-            width: 200,
-            height: 50,
-            borderRadius: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: 20,
-          }}
-          onPress={() => {
-            navigation.navigate('Home', { player: 'O' });
-          }}
+          style={styles.pinkButton}
+          onPress={() => handleButtonPress('O')}
         >
-          <Text style={{ fontSize: 20, color: '#fff', fontWeight: 'bold' }}>
-            AI goes first
-          </Text>
+          <Text style={styles.buttonLabel}>AI goes first</Text>
         </TouchableOpacity>
         <TouchableOpacity
           accessibilityRole="button"
-          style={{
-            backgroundColor: '#BF00FF',
-            width: 200,
-            height: 50,
-            borderRadius: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: 20,
-          }}
-          onPress={() => {
-            navigation.navigate('Home', { player: 'X' });
-          }}
+          style={styles.purpleButton}
+          onPress={() => handleButtonPress('X')}
         >
-          <Text style={{ fontSize: 20, color: '#fff', fontWeight: 'bold' }}>
-            You go first
-          </Text>
+          <Text style={styles.buttonLabel}>You go first</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centeredImage: {
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  pinkButton: {
+    backgroundColor: '#FF6EC7',
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  purpleButton: {
+    backgroundColor: '#BF00FF',
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  buttonLabel: { fontSize: 20, color: '#fff', fontWeight: 'bold' },
+});
